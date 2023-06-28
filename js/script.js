79dbe2c4a4debf71ea892e49509a13ee4d7458fb
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-28 16:42:34 +0200

    Aggiunto array di oggetti

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Creare array di oggetti con le info fornite 
+
+const team = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        img : 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        img : 'walter-gordon-office-manager.jpg',
+    },{
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        img : 'angela-lopez-social-media-manager.jpg',
+    },{
+        name : 'Scott Estrada',
+        role : 'Developer',
+        img : 'scott-estrada-developer.jpg',
+    },{
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        img : 'barbara-ramos-graphic-designer.jpg',
+    }
+];
+
+
+
+
+// Dati forniti 
+// NOME            RUOLO                   IMMAGINE
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
